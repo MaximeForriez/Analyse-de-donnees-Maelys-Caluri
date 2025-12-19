119c1b5985e612fdaf4df03cdaccf2e378ea1fa8
MaelysCaluri <maelys.caluri@gmail.com>
2025-12-19 19:40:30 +0100

    mise a jour seance 4

--- Seance-04/src/main.py
+++ Seance-04/src/main.py
@@ -12,11 +12,12 @@ import math
 
 dist_names = ['norm', 'beta', 'gamma', 'pareto', 't', 'lognorm', 'invgamma', 'invgauss',  'loggamma', 'alpha', 'chi', 'chi2', 'bradford', 'burr', 'burr12', 'cauchy', 'dweibull', 'erlang', 'expon', 'exponnorm', 'exponweib', 'exponpow', 'f', 'genpareto', 'gausshyper', 'gibrat', 'gompertz', 'gumbel_r', 'pareto', 'pearson3', 'powerlaw', 'triang', 'weibull_min', 'weibull_max', 'bernoulli', 'betabinom', 'betanbinom', 'binom', 'geom', 'hypergeom', 'logser', 'nbinom', 'poisson', 'poisson_binom', 'randint', 'zipf', 'zipfian']
 
+###
+# DEFINITION DES FONCTIONS LOCALES
+###
 
-## 1 - VISUALISER DES DISTRIBUTIONS
-## 1.A - Distributions statistiques de Variables DISCRETES
-
-## Question 1.A.1 - Loi de Dirac
+## Distributions statistiques de Variables DISCRETES
+## 1.A.1 - Loi de Dirac
 # Dirac = "Une seule issue est probable à 100%."
 def distrib_dirac_discret(a, x_min, x_max):
     X = []
@@ -30,10 +31,121 @@ def distrib_dirac_discret(a, x_min, x_max):
 
     return X, P
 
+## 1.A.2 - Loi Uniforme Discrète
+# Uniforme : "Toutes les issues sont équiprobables."
+def distrib_uniforme_discret(x_min, x_max):
+    X = []
+    P = []
+
+    n = (x_max - x_min) + 1   # n = nombre de valeurs possibles
+    for i in range(x_min, x_max+1):
+        X.append(i)
+        P.append(1/n)         # 1/n = probabilité pour chacune
+
+    return X, P
+
+## 1.A.3 - Loi Binomiale
+def distrib_binomiale_discret(n, p):
+    K = []
+    for k in range(0, n+1):
+        K.append(k)
+
+    P = scipy.stats.binom.pmf(K, n, p)
+
+    return K, P
+
+
+## 1.A.4 - Loi de Poisson
+def distrib_poisson_discret(lam, n_max):
+    K = []
+    for k in range(0, n_max +1):
+        K.append(k)
+
+    P = scipy.stats.poisson.pmf(K, lam)
+
+    return K, P
+
+
+## 1.A.5 - Loi de Zipf-Mandelbrot
+def distrib_zipf_mandelbrot(s, q, N):
+    X = []
+    P = []
+    # Calcul de la constante de normalisation H
+    H = 0
+    for i in range(0, N+1):
+        H += 1.0 / ((i + q)**s)
+    # Fin du calcul de H
+
+    for i in range(0, N+1):
+        X.append(i)
+        P.append(1.0 / (H * ((i + q)**s)))
+
+    return X, P
+
+## Distributions statistiques de Variables CONTINUES
+## 1.B.1 - Loi de Poisson
+def distrib_poisson_continu(mu, n_max, finesse):
+    K_cont = np.linspace(0, n_max, finesse)
+    P_cont = []
+    for i in range(0, len(K_cont)):
+        k = K_cont[i]
+        pr_k = math.exp(-mu) * (mu**k) / (math.gamma(k+1))
+        P_cont.append(pr_k)
+
+    return K_cont, P_cont
+
+## 1.B.2 - Loi Normale
+# Loi Normale : "La fameuse Gaussienne"
+def distrib_normale_continu(mu, sigma, facteur_sigma = 3, finesse = 500):
+    K_cont = np.linspace(mu - facteur_sigma*sigma, mu + facteur_sigma*sigma, finesse) # la largeur de l'intervalle est fixée ici
+    P_cont = scipy.stats.norm.pdf(K_cont, mu, sigma)
+
+    return K_cont, P_cont
+
+## 1.B.3 - Loi Log-normale
+def distrib_normale_continu(mu, sigma, n_max, finesse = 500):
+    K_cont = np.linspace(0.001, n_max, finesse) 
+    P_cont = scipy.stats.lognorm.pdf(K_cont, s=sigma, scale=np.exp(mu))
+
+    return K_cont, P_cont
+
+## 1.B.4 - Loi Uniforme
+def distrib_uniforme_continu(x_min, a, b, x_max, finesse = 500):
+    K_cont = np.linspace(x_min, x_max, finesse)
+    P_cont = scipy.stats.uniform.pdf(K_cont, loc=a, scale=b-a)
+
+    return K_cont, P_cont
+
+## 1.B.5 - Loi du Chi²
+def distrib_chi2_continu(k, n_max, finesse = 500):
+    K_cont = np.linspace(0.001, n_max, finesse)
+    P_cont = scipy.stats.chi2.pdf(K_cont, df=k)
+
+    return K_cont, P_cont
+
+## Question 1.B.6 - Loi de Pareto
+def distrib_pareto_continu(alpha, x_m, n_max, finesse = 500):
+    K_cont = np.linspace(x_m, n_max, finesse)
+    P_cont = scipy.stats.pareto.pdf(K_cont, b=alpha, scale=x_m)
+
+    return K_cont, P_cont
+
+
+
+
+
+
+
+## 1 - VISUALISER DES DISTRIBUTIONS
+## 1.A - Distributions statistiques de Variables DISCRETES
+
+## Question 1.A.1 - Loi de Dirac
+# Dirac = "Une seule issue est probable à 100%."
+
 # Paramètrage
-a = 5
-x_min = 1
-x_max = 6
+a = 7
+x_min = 0
+x_max = 20
 
 # On génère la distribution
 X, P = distrib_dirac_discret(a, x_min, x_max)
@@ -53,16 +165,6 @@ plt.close()
 
 ## Question 1.A.2 - Loi Uniforme Discrète
 # Uniforme : "Toutes les issues sont équiprobables."
-def distrib_uniforme_discret(x_min, x_max):
-    X = []
-    P = []
-
-    n = (x_max - x_min) + 1   # n = nombre de valeurs possibles
-    for i in range(x_min, x_max+1):
-        X.append(i)
-        P.append(1/n)         # 1/n = probabilité pour chacune
-
-    return X, P
 
 # Paramètrage
 x_min = 1
@@ -85,19 +187,10 @@ plt.close()
 
 
 
-
 ## Question 1.A.3 - Loi Binomiale
 # Binomiale : "Je joue à pile ou face 57 fois, 
 #              combien j'ai de chance de remporter au total:
 #              0 manche? 1 manche? 2 manches? ... 57 manches?"
-def distrib_binomiale_discret(n, p):
-    K = []
-    for k in range(0, n+1):
-        K.append(k)
-
-    P = scipy.stats.binom.pmf(K, n, p)
-
-    return K, P
 
 # Paramètrage
 n = 20    # Nombre d'épreuves
@@ -119,6 +212,7 @@ plt.close()
 
 
 
+
 ## Question 1.A.4 - Loi de Poisson
 # Poisson : "Même philosophie que Binomiale, mais quand événement rare."
 #           Exemple : Hypothèse = il y a en moyenne 2 etoiles filantes chaque 5 min.
@@ -126,14 +220,6 @@ plt.close()
 #                     Maintenant on choisit dans la soirée une tranche de 5 min :
 #                        Alors Poisson donne la probabilité qu'il y passe :
 #                        0 comète / 1 comète / 2 comètes / ... / 14 980 comètes / ...
-def distrib_poisson_discret(lam, n_max):
-    K = []
-    for k in range(0, n_max +1):
-        K.append(k)
-
-    P = scipy.stats.poisson.pmf(K, lam)
-
-    return K, P
 
 # Paramètrage
 lam = 5     # Paramètre lambda
@@ -146,12 +232,11 @@ K, P = distrib_poisson_discret(lam, n_max)
 plt.stem(K, P)
 plt.title("Loi de Poisson de paramètre lambda = {}.".format(lam))
 plt.xlabel("k")
-plt.xticks(K[::5])
+plt.xticks(K)
 plt.ylabel("Pr(X = k)")
 plt.grid(True, linestyle="--", alpha=0.5)
 plt.savefig("./output/1_A_Distrib_Discretes/4_Poisson_discret.png")
 plt.close()
-
 
 
 
@@ -173,20 +258,6 @@ plt.close()
 # 
 # N.B.: Zipf-Mandelbrot se retrouve souvent en géographie !
 #       Exemple : classement des populations des villes.
-def distrib_zipf_mandelbrot(s, q, N):
-    X = []
-    P = []
-    # Calcul de la constante de normalisation H
-    H = 0
-    for i in range(0, N+1):
-        H += 1.0 / ((i + q)**s)
-    # Fin du calcul de H
-
-    for i in range(0, N+1):
-        X.append(i)
-        P.append(1.0 / (H * ((i + q)**s)))
-
-    return X, P
 
 # Paramètrage
 s = 1.5  # Exposant (doit être > 1)
@@ -209,18 +280,8 @@ plt.close()
 
 
 
-
 ## 1.B - Distributions statistiques de Variables CONTINUES
 ## Question 1.B.1 - Loi de Poisson
-def distrib_poisson_continu(mu, n_max, finesse):
-    K_cont = np.linspace(0, n_max, finesse)
-    P_cont = []
-    for i in range(0, len(K_cont)):
-        k = K_cont[i]
-        pr_k = math.exp(-mu) * (mu**k) / (math.gamma(k+1))
-        P_cont.append(pr_k)
-
-    return K_cont, P_cont
 
 # Paramètrage
 mu = 5         # Paramètre mu
@@ -250,17 +311,14 @@ plt.savefig("./output/1_B_Distrib_Continues/1_Poisson_continu_remplie.png")
 plt.close()
 
 
+
+
 ## Question 1.B.2 - Loi Normale
 # Loi Normale : "La fameuse Gaussienne"
 #                ... centrée en la valeur mu
 #                ... s'étend de - infini à + infini
 #                ... mais 99.7% se trouvent entre mu - 3 sigma 
 #                                              et mu + 3 sigma
-def distrib_normale_continu(mu, sigma, facteur_sigma = 3, finesse = 500):
-    K_cont = np.linspace(mu - facteur_sigma*sigma, mu + facteur_sigma*sigma, finesse) # la largeur de l'intervalle est fixée ici
-    P_cont = scipy.stats.norm.pdf(K_cont, mu, sigma)
-
-    return K_cont, P_cont
 
 # Paramètrage
 mu = 5             # Paramètre mu, valeur centrale
@@ -296,11 +354,6 @@ plt.close()
 
 
 ## Question 1.B.3 - Loi Log-normale
-def distrib_normale_continu(mu, sigma, n_max, finesse = 500):
-    K_cont = np.linspace(0.001, n_max, finesse) 
-    P_cont = scipy.stats.lognorm.pdf(K_cont, s=sigma, scale=np.exp(mu))
-
-    return K_cont, P_cont
 
 # Paramètrage
 mu = 1             # Paramètre mu
@@ -334,11 +387,6 @@ plt.close()
 
 
 ## Question 1.B.4 - Loi Uniforme
-def distrib_uniforme_continu(x_min, a, b, x_max, finesse = 500):
-    K_cont = np.linspace(x_min, x_max, finesse)
-    P_cont = scipy.stats.uniform.pdf(K_cont, loc=a, scale=b-a)
-
-    return K_cont, P_cont
 
 # Paramètrage
 x_min = -10    # borne sup de la fenêtre
@@ -373,15 +421,10 @@ plt.close()
 
 
 ## Question 1.B.5 - Loi du Chi²
-def distrib_chi2_continu(k, n_max, finesse = 500):
-    K_cont = np.linspace(0.001, n_max, finesse)
-    P_cont = scipy.stats.chi2.pdf(K_cont, df=k)
-
-    return K_cont, P_cont
 
 # Paramètrage
 k = 5          # Paramètre k
-n_max = 50    # Valeur maximale
+n_max = 50     # Valeur maximale
 finesse = 500  # nombre de points
 
 # On génère la distribution
@@ -409,16 +452,10 @@ plt.close()
 
 
 ## Question 1.B.6 - Loi de Pareto
-def distrib_pareto_continu(alpha, x_m, n_max, finesse = 500):
-    K_cont = np.linspace(x_m, n_max, finesse)
-    P_cont = scipy.stats.pareto.pdf(K_cont, b=alpha, scale=x_m)
-
-    return K_cont, P_cont
-
 
 # paramètres
 alpha = 1      # shape
-x_m = 1          # x_m
+x_m = 1        # x_m
 n_max = 20
 finesse = 500  # nombre de points
 
@@ -443,6 +480,9 @@ plt.ylabel("Densité de probabilité f(x)")
 plt.grid(True, linestyle="--", alpha=0.5)
 plt.savefig("./output/1_B_Distrib_Continues/6_Pareto_continu_remplie.png")
 plt.close()
+
+
+
 
 ## 2 - CALCUL MOYENNE ET ECART-TYPE
 # Définition des fonctions
